1b6dbb633d6f51a03a8d98a2b52f6ce8d7a5541b
stavShukrun <84853971+stavShukrun@users.noreply.github.com>
2022-01-02 16:24:06 +0200

    Add files via upload

---
+++ webdriverTests/tests/test_web.py
@@ -0,0 +1,23 @@
+import pytest
+from pages.result import DuckDuckGoResultPage
+from pages.search import DuckDuckGoSearchPage
+from selenium.webdriver import Chrome
+
+@pytest.fixture
+def browser():
+    driver = Chrome()
+    driver.implicitly_wait(20)
+    yield driver
+    driver.quit()
+
+def test_baisc_duckduckgo_search(browser):
+    PHRASE= 'panda'
+
+    search_page = DuckDuckGoSearchPage(browser)
+    search_page.load()
+    search_page.search(PHRASE)
+
+    result_page = DuckDuckGoResultPage(browser)
+    assert result_page.link_div_count() > 0
+    assert result_page.phrase_result_count(PHRASE) > 0 
+    assert result_page.search_input_value() == PHRASE
